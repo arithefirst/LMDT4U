ca1530de8316579a1bde8797041a4dbf1db9a179
April Hall <ari@arithefirst.com>
2025-03-13 15:38:38 -0400

    build: Setup TailwindCSS

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  const { children }: { children: Snippet } = $props();
+
+  // Load Tailwind
+  import '$lib/app.css' 
+</script>
+
+{@render children()}
