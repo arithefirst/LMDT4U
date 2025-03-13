3d1fa3d9be16d37a47382e2d0a9ad5cf425f81f0
April Hall <ari@arithefirst.com>
2025-03-13 16:34:54 -0400

    style: Adjust print width

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,10 @@
 <main class="bg-base text-text flex h-screen w-screen flex-col items-center justify-center">
   {@render children()}
   <footer class="absolute bottom-2 w-full text-center text-xs">
-    The DuckDuckGo name, logos, trade dress, and other brand elements are trademarks of Duck Duck Go, Inc. For more information, see
-    <a href="/ddg-brand-guidelines.pdf" target="_blank" class="text-sky-500 underline hover:text-sky-600">ddg-brand-guidelines.pdf</a>
+    The DuckDuckGo name, logos, trade dress, and other brand elements are trademarks of Duck Duck Go, Inc. For more
+    information, see
+    <a href="/ddg-brand-guidelines.pdf" target="_blank" class="text-sky-500 underline hover:text-sky-600"
+      >ddg-brand-guidelines.pdf</a
+    >
   </footer>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,7 @@
 
 <div class="bg-searchbar flex h-[42px] w-11/12 max-w-[620px] items-center justify-center rounded-lg px-3">
   <input class="h-[42px] flex-grow" />
-  <button class="h-[42px] cursor-pointer p-1.5 text-[#A5A5A5]" onclick={() => console.log('search')}><Search size={18} /></button>
+  <button class="h-[42px] cursor-pointer p-1.5 text-[#A5A5A5]" onclick={() => console.log('search')}
+    ><Search size={18} /></button
+  >
 </div>
