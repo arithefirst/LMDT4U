25f70d2ea8e5fc33830354baceea8b85e802e08b
April Hall <ari@arithefirst.com>
2025-03-13 16:46:10 -0400

    fix: Make search bar more accurate to start.duckduckgo.com

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,10 @@
   import { Search } from 'lucide-svelte';
 </script>
 
-<div class="bg-searchbar flex h-[42px] w-11/12 max-w-[620px] items-center justify-center rounded-lg px-3">
-  <input class="h-[42px] flex-grow" />
-  <button class="h-[42px] cursor-pointer p-1.5 text-[#A5A5A5]" onclick={() => console.log('search')}
-    ><Search size={18} /></button
+<div class="bg-searchbar flex h-[42px] w-11/12 max-w-[620px] items-center justify-center rounded-lg">
+  <input class="caret-ddg-blue50 h-[42px] flex-grow pl-3 outline-none" />
+  <button
+    class="bg-ddg-blue30 hover:bg-ddg-blue40 text-text-dark h-[42px] cursor-pointer rounded-r-lg px-4 py-1.5"
+    onclick={() => console.log('search')}><Search size={18} /></button
   >
 </div>
