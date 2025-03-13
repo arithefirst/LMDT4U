f6897243950c5579dcd95457dad299638aa8d9ef
April Hall <ari@arithefirst.com>
2025-03-13 17:00:48 -0400

    feat: Clear searchbar button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
-  import { Search } from 'lucide-svelte';
+  import { Search, X as X_Icon } from 'lucide-svelte';
+
+  let value: string = $state('');
 </script>
 
 <div class="bg-searchbar flex h-[42px] w-11/12 max-w-[620px] items-center justify-center rounded-lg">
-  <input class="caret-ddg-blue50 h-[42px] flex-grow pl-3 outline-none" />
+  <input bind:value class="caret-ddg-blue50 h-[42px] flex-grow pl-3 outline-none" />
+  {#if value.length > 0}
+    <button class="hover:text-text h-[42px] cursor-pointer p-2 text-[#999999]" onclick={() => (value = '')}
+      ><X_Icon size={18} /></button
+    >
+  {/if}
   <button
     class="bg-ddg-blue30 hover:bg-ddg-blue40 text-text-dark h-[42px] cursor-pointer rounded-r-lg px-4 py-1.5"
     onclick={() => console.log('search')}><Search size={18} /></button
