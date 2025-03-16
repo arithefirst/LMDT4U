731219f8fe52387c69c46ccadff0d92f74287521
April Hall <ari@arithefirst.com>
2025-03-15 21:57:12 -0400

    feat: License info in footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,22 @@
+<footer class="absolute bottom-2 w-full px-3 text-center text-xs">
+  <p>
+    Developed by <a
+      href="https://www.arithefirst.com"
+      target="_blank"
+      class="text-sky-500 underline hover:text-sky-600">April Hall</a
+    >
+    under the
+    <a
+      href="https://www.gnu.org/licenses/gpl-3.0.en.html"
+      target="_blank"
+      class="text-sky-500 underline hover:text-sky-600">GNU General Public License 3.0</a
+    >.
+  </p>
+  <p>
+    The DuckDuckGo name, logos, trade dress, and other brand elements are trademarks of Duck Duck Go, Inc. For more
+    information, see
+    <a href="/ddg-brand-guidelines.pdf" target="_blank" class="text-sky-500 underline hover:text-sky-600"
+      >ddg-brand-guidelines.pdf</a
+    >
+  </p>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,19 @@
 <script lang="ts">
+  import Footer from '$lib/components/footer.svelte';
+  import Meta from '$lib/components/meta.svelte';
   import type { Snippet } from 'svelte';
   import { Toaster } from 'svelte-sonner';
-  import Meta from '$lib/components/meta.svelte';
-  const { children }: { children: Snippet } = $props();
 
   // Load Tailwind
   import '$lib/app.css';
+
+  const { children }: { children: Snippet } = $props();
 </script>
 
 <Meta />
 <main class="bg-base text-text flex h-screen w-screen flex-col items-center justify-center">
   {@render children()}
-  <footer class="absolute bottom-2 w-full text-center text-xs">
-    The DuckDuckGo name, logos, trade dress, and other brand elements are trademarks of Duck Duck Go, Inc. For more
-    information, see
-    <a href="/ddg-brand-guidelines.pdf" target="_blank" class="text-sky-500 underline hover:text-sky-600"
-      >ddg-brand-guidelines.pdf</a
-    >
-  </footer>
+  <Footer />
 </main>
 <Toaster
   position="bottom-center"
